ef0c6ae61cb614faf2d92e79ec353d117851d1e8
70Cloud <1153438124@qq.com>
2019-07-10 22:24:50 +0800

    commit a demo

--- stage-5/lesson-17/src/main/java/deep/in/java/ClassLoaderDemo.java
+++ stage-5/lesson-17/src/main/java/deep/in/java/ClassLoaderDemo.java
@@ -21,6 +21,16 @@ public class ClassLoaderDemo {
         System.out.println(Thread.currentThread().getContextClassLoader());
 
         // 如何实现类隔离，通过修改 Thread 上下文 ClassLoader
+
+        // ClassLoader previousClassLoader = Thread.currentThread().getContextLoader();
+        // previousClassLoader 能够加载 User.class V1 版本（user-api-1.0.0.jar 文件中，在 /classpath1 目录下）
+        // User.class V2 版本 （user-api-2.0.0.jar 文件中，在 /classpath2 目录下）
+        // loadUser 操作加载 User.class V2 版本
+
+        // previousClassLoader ClassPath -> /classpath1
+        // newClassLoader  ClassPath -> /classpath2
+
+        // 通常，系统或者应用（包括自定义） ClassLoader 均为 URLClassLoader 子类
     }
 
     private static void changeClassLoader(ClassLoader newClassLoader){
@@ -40,12 +50,22 @@ public class ClassLoaderDemo {
 
     private static void loadSomeClasses(){// 兼容或适配老的 ClassLoader 代码
         // JAXB 通过线程上下文 ClassLoader 切换不同实现 SPI
+        // JAXB 1.x 2.x
+        // JDK 提供的 API 1.x
+        // 第三方包实现 2.x
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
         try{
-            classLoader.loadClass("abc");
+            classLoader.loadClass("User"); // 使用 V2
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 }
+
+//class User { // V1
+//
+//}
+
+// Class User { // V2
+// }
