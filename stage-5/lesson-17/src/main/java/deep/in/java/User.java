9e102083f2c9f18a5d04f9a650f03ea21d909177
70Cloud <1153438124@qq.com>
2019-07-11 22:43:59 +0800

    commit a demo

---
+++ stage-5/lesson-17/src/main/java/deep/in/java/User.java
@@ -0,0 +1,19 @@
+package deep.in.java;
+
+/**
+ * @Author: NoNo
+ * @Description: V1 版本有 ID 字段
+ * @Date: Create in 22:33 2019/7/11
+ */
+public class User {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
